dd08ff4e7d3f11aaf26b546c8acf9cd5e6541b56
Ryan Kretch <rkretch91@gmail.com>
2020-05-20 09:18:35 +0200

    database seeded with restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Restaurants being destroyed"
+Restaurant.destroy_all
+
+100.times do
+  name = Faker::Hipster.word
+  address = Faker::Address.street_address
+  rating = rand(1..5)
+
+  Restaurant.create(name: name, address: address, rating: rating)
+  puts "Restaurant #{name} created"
+end
+
+puts "All restaurants created"
